6322218323dbb06282164835f531cc9309e6bfce
codesqills <codesqills@gmail.com>
2017-10-23 23:43:23 -0400

    Academic calendar implementation

--- app/src/main/java/edu/thapar/newindialms/OpenCalendar.java
+++ app/src/main/java/edu/thapar/newindialms/OpenCalendar.java
@@ -1,9 +1,12 @@
 package edu.thapar.newindialms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
@@ -14,6 +17,7 @@ import android.widget.TextView;
 public class OpenCalendar extends AppCompatActivity {
     private static final String TAG = "OpenCalendar";
     TextView displayDate;
+    Button open_calendar_details;
     private CalendarView calendarView;
 
     @Override
@@ -22,6 +26,15 @@ public class OpenCalendar extends AppCompatActivity {
         setContentView(R.layout.academic_calendar_layout);
         calendarView = (CalendarView) findViewById(R.id.calendarView);
         displayDate = (TextView) findViewById(R.id.calendar_displaydate);
+        open_calendar_details = (Button) findViewById(R.id.open_calendar_details);
+
+        open_calendar_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent calendarintent= new Intent(getApplicationContext(), AcademicCalendar.class);
+                startActivity(calendarintent);
+            }
+        });
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
