c0675e7f98593c5a2f3b56560487a4373b2ee281
codesqills <codesqills@gmail.com>
2017-11-14 13:13:07 -0500

    Faculty Dashboard Implementation

---
+++ app/src/main/java/edu/thapar/newindialms/StudentDashboard.java
@@ -0,0 +1,26 @@
+package edu.thapar.newindialms;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import static edu.thapar.newindialms.R.id.faculty_toolbar_title;
+
+public class FacultyDashboard extends AppCompatActivity {
+    Toolbar toolbar_main_screen;
+    MenuItem menuItem;
+    String facultyname,facultyid;
+    TextView faculty_toolbar_name,faculty_toolbar_id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facultydashboard);
+
+    }
+
+
+
+}
